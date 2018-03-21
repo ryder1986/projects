078da29a8fb40aec8c1a68b0328b55e13b9cdec2
root <you@example.com>
2018-03-21 11:32:18 +0800

    1

---
+++ demo2-v1/demo2/client/serverinfosearcher.h
@@ -0,0 +1,141 @@
+#ifndef SERVERINFOSEARCHER_H
+#define SERVERINFOSEARCHER_H
+
+#include <QObject>
+#include <QtNetwork/QUdpSocket>
+#include "tool1.h"
+#include <QThread>
+#include "pd.h"
+class ServerReplyCheckRouting : public QObject
+{
+    Q_OBJECT
+
+public slots:
+    void check_reply(    QUdpSocket *udp_skt_find_server) {
+        QString str;
+        str.clear();
+        int try_times=100;
+        while(try_times--){
+            if(udp_skt_find_server->hasPendingDatagrams()){
+                datagram.resize((udp_skt_find_server->pendingDatagramSize()));
+                udp_skt_find_server->readDatagram(datagram.data(),datagram.size());
+                prt(info,"get server info : %s",datagram.data());
+                server_ip.clear();
+                server_ip.append(datagram.split(',')[0]);
+                prt(info,"ip : %s",server_ip.toStdString().data());
+                ip_list.append(server_ip);
+                emit resultReady(server_ip);
+            }else{
+                //      prt(info,"searching");
+            }
+            QThread::msleep(10);
+        }
+    }
+
+signals:
+    void resultReady(  QString result);
+private:
+    //  QUdpSocket *udp_skt_find_server;
+
+    QByteArray datagram;
+    QString server_ip;
+    QStringList ip_list;
+};
+
+class ServerInfoSearcher : public QObject{
+    Q_OBJECT
+    QThread check_thread;
+    ServerReplyCheckRouting *p_checker;
+
+public:
+    ServerInfoSearcher()
+    {
+
+        udp_skt_find_server=new QUdpSocket(this);
+        udp_skt_find_server->bind(Protocol::CLIENT_REPORTER_PORT,QUdpSocket::ShareAddress);
+        //     connect(udp_skt_find_server,SIGNAL(readyRead()),this,SLOT(get_reply()),Qt::QueuedConnection);
+
+        //   connect();
+
+        p_checker=new ServerReplyCheckRouting;
+        p_checker->moveToThread(&check_thread);
+        connect(&check_thread,&QThread::finished,p_checker,&QObject::deleteLater);
+        connect(this,SIGNAL(begin_search(QUdpSocket*)),p_checker,SLOT(check_reply(QUdpSocket*)),Qt::QueuedConnection);
+        connect(p_checker,SIGNAL(resultReady(QString)),this,SLOT(ip_found(QString)),Qt::QueuedConnection);
+    }
+    ~ServerInfoSearcher()
+    {
+        check_thread.quit();
+        check_thread.wait();
+    }
+    void broadcast_info()
+    {
+        QByteArray b;
+        b.append("pedestrian");
+        udp_skt_find_server->writeDatagram(b.data(), b.size(),
+                                           QHostAddress::Broadcast, Protocol::SERVER_REPORTER_PORT);
+        prt(info,"finding server ...");
+
+    }
+    void search()
+    {
+
+        check_thread.start();
+        emit begin_search(udp_skt_find_server);
+    }
+
+    void search_device()
+    {
+        ip_list.clear();
+        broadcast_info();
+        search();
+        //   p_find_server_thread=new std::thread(find_server);
+        //   QThread::msleep(3000);
+
+    }
+    QStringList search_rst()
+    {
+        return ip_list;
+    }
+    static void find_server()
+    {
+        prt(info," find server thread");
+        int times=10;
+        while(times--){
+
+        }
+    }
+signals:
+    void begin_search( QUdpSocket *udp_skt_find_server);
+    void find_ip(QString ip);
+
+public slots:
+    void ip_found(QString ip)
+    {
+        qDebug()<<ip;
+       // ip_list.append(ip);
+        emit find_ip(ip);
+    }
+
+//    void get_reply()
+//    {
+//        //  while(udp_skt->hasPendingDatagrams())
+//        if(udp_skt_find_server->hasPendingDatagrams())
+//        {
+//            datagram.resize((udp_skt_find_server->pendingDatagramSize()));
+//            udp_skt_find_server->readDatagram(datagram.data(),datagram.size());
+//            prt(info,"get server info : %s",datagram.data());
+//            server_ip.append(datagram.split(',')[0]);
+//            ip_list.append(server_ip);
+//        }
+//    }
+
+private :
+    QUdpSocket *udp_skt_find_server;
+
+    QByteArray datagram;
+    QString server_ip;
+    QStringList ip_list;
+};
+
+#endif // SERVERINFOSEARCHER_H
